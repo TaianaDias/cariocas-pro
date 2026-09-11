f975a9c8c6d3e971aaa3b60f50bbc788ff9a6f65
TaianaDias <57508229+TaianaDias@users.noreply.github.com>
2026-09-10 23:49:18 -0300

    security: separar producao administrativa e operacional

--- src/app/producao/page.tsx
+++ src/app/producao/page.tsx
@@ -1,5 +1,21 @@
+"use client";
+
 import { ProducaoPageClient } from "../../components/operacional/OperationalPages";
+import { ProducaoOperacional } from "../../components/producao/ProducaoOperacional";
+import { Spinner } from "../../components/ui/Spinner";
+import { useAuth } from "../../hooks/useAuth";
+import { isOperationalRole } from "../../lib/access-control";
 
 export default function ProducaoPage() {
+  const { loading, userProfile } = useAuth();
+
+  if (loading || !userProfile) {
+    return <Spinner />;
+  }
+
+  if (isOperationalRole(userProfile.role)) {
+    return <ProducaoOperacional />;
+  }
+
   return <ProducaoPageClient />;
 }
