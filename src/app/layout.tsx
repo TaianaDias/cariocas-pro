75099e824f1f0c3f70748dca41f329326867259b
TaianaDias <57508229+TaianaDias@users.noreply.github.com>
2026-09-15 12:35:13 -0300

    style: carregar estilos de validades

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,7 @@ import "../styles/precificacao.css";
 import "../styles/operacional.css";
 import "../styles/processos.css";
 import "../styles/pedidos.css";
+import "../styles/relatorios-validades.css";
 import "../styles/carioquinha.css";
 import "../styles/auth.css";
 import "../styles/auditoria.css";
